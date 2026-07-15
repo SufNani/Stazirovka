433dffead2b02bcde8dfa5f0fa502c8b61339fcc
vsevolod.rachis <seva-ra4is@mail.ru>
2026-07-15 15:54:10 +0700

    fix allowedHosts

--- stazirovka-proba-pera/vite.config.js
+++ stazirovka-proba-pera/vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    host: '0.0.0.0',
+    allowedHosts: ['kalendart.ddns.net'],
+    port: 5173,
+  },
 })
